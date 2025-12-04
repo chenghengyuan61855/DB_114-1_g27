47134f027af8377ee90d860cec850ffaf6fd38ee
chenghengyuan61855 <chenghengyuan61855@gmail.com>
2025-12-05 03:42:29 +0800

    fix cancel_check()

--- ui/helper.py
+++ ui/helper.py
@@ -1,5 +1,5 @@
 def cancel_check(input_str: str, ui_type: str) -> bool:
-    if input_str.strip() == ":q":
+    if input_str.strip() == "q":
         print(f"{ui_type} cancelled.")
         return True
     return False
