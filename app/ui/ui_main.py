250758657019ce74caea23fcb5f53546247e8366
chenghengyuan61855 <chenghengyuan61855@gmail.com>
2025-12-05 01:42:43 +0800

    Finish ui_main flow

--- app/ui/ui_main.py
+++ app/ui/ui_main.py
@@ -1,2 +1,20 @@
+from app.ui.ui_user import ui_login_user, ui_create_user
+
 def run():
     print("Welcome to DaTeabase 🍹")
+    print("=====================")
+    print("1. Login")
+    print("2. Create User")
+    print("q. Quit")
+    while True:
+        command = input("Enter command: ").strip()
+        if command == "1":
+            user_id = ui_login_user()
+            # TODO: go to menu.main_menu.py if login successful
+        elif command == "2":
+            ui_create_user()
+        elif command == "q":
+            print("Goodbye!")
+            return
+        else:
+            print("Invalid command. Please try again.")
