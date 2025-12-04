1a21473f838c66e6d4b084890ae22fb544fe7cf3
chenghengyuan61855 <chenghengyuan61855@gmail.com>
2025-12-04 11:26:17 +0800

    Add db_conn

--- db_conn.py
+++ db_conn.py
@@ -1,20 +1,26 @@
 import psycopg2
 import os
+from dotenv import load_dotenv
 
 DB_NAME = "DaTeabase"
 DB_USER = "postgres"
-DB_PASSWORD = os.getenv("DB_PASSWORD") #Powershell: $env:DB_PASSWORD="your_password"
 DB_HOST = "localhost"
 DB_PORT = 5432
+
+def _get_db_password():
+    load_dotenv()
+    pwd = os.getenv("DB_PASSWORD")
+    if not pwd:
+        raise RuntimeError("DB_PASSWORD is not set")
+    return pwd
 
 db = None
 cur = None
 
 def connect():
     global db, cur
-    if DB_PASSWORD is None:
-        raise RuntimeError("DB_PASSWORD is not set")
     try:
+        DB_PASSWORD = _get_db_password()
         db = psycopg2.connect(
             dbname=DB_NAME,
             user=DB_USER,
