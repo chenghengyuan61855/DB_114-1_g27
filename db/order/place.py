4a976b7b3f87842676bda4a9c5ecaf406bc2bfd2
chenghengyuan61855 <chenghengyuan61855@gmail.com>
2025-12-06 18:50:48 +0800

    Add place_order()

---
+++ db/order/place.py
@@ -0,0 +1,37 @@
+from db.crud import fetch, update, insert
+
+def db_place_order_pickup(user_id, store_id, total_price, payment_method, payment_status):
+
+    order_data = {
+        "user_id": user_id,
+        "store_id": store_id,
+        "order_status": "placed",
+        "order_type": "pickup",
+        "total_price": total_price,
+        "payment_method": payment_method,
+        "payment_status": payment_status,
+    }
+
+
+    order_id = insert("ORDERS", order_data)
+    return order_id
+
+def db_place_order_delivery(user_id, store_id, total_price, payment_method, payment_status,
+                            delivery_address, reciver_name, reciver_phone):
+
+    order_data = {
+        "user_id": user_id,
+        "store_id": store_id,
+        "order_status": "placed",
+        "order_type": "delivery",
+        "delivery_address": delivery_address,
+        "reciver_name": reciver_name,
+        "reciver_phone": reciver_phone,
+        "total_price": total_price,
+        "payment_method": payment_method,
+        "payment_status": payment_status,
+    }
+
+
+    order_id = insert("ORDERS", order_data)
+    return order_id
